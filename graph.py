cc932d6590101a04218d9f66d1abf33d170d4b25
Jeffrey Katz <JeffreyEricKatz@gmail.com>
2018-02-07 11:10:00 -0500

    add line_trace function

--- graph.py
+++ graph.py
@@ -1,10 +1,14 @@
 import plotly
 from plotly.offline import iplot
 
-def trace(data, markers = True):
+def trace(data, mode = 'markers'):
     x_values = list(map(lambda point: point['x'],data))
     y_values = list(map(lambda point: point['y'],data))
-    return {'x': x_values, 'y': y_values, 'mode': 'markers'}
+    return {'x': x_values, 'y': y_values, 'mode': mode}
 
-def scatter_plot(traces):
+def line_function_trace(line_function, x_values, mode = 'line'):
+    y_values = list(map(lambda x: line_function(x), x_values))
+    return {'x': x_values, 'y': y_values, 'mode': mode}
+
+def plot(traces):
     plotly.offline.iplot(traces)
